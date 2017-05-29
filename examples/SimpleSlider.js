9fa31fa844ec93ffbbfbf2ad1f35a0a0127ce9a8
Konrad Marzec <kmarzec07@gmail.com>
2017-05-29 15:49:50 +0200

    commented unneccessary code - resolves problem with setState

--- examples/SimpleSlider.js
+++ examples/SimpleSlider.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 import Slider from '../src/slider'
 
 export default class SimpleSlider extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     const settings = {
       dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      beforeChange: idx => this.setState({ idx }),
     };
+
     return (
       <div>
         <h2> Single Item</h2>
